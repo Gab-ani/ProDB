bdd71a0a87b4cf8ba6de3505bd5605fae96ed342
Gab-ani <grisha.b2010@yandex.ru>
2022-08-19 14:29:00 +0300

    getting match info from stratz

--- ProDB/src/main/java/data/Match.java
+++ ProDB/src/main/java/data/Match.java
@@ -1,29 +1,88 @@
 package data;
 
+import java.util.ArrayList;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 @Entity
 public class Match {
 
 	@Id
-	private int matchId;
+	private long matchId;
 	 
 	private String date;
 	
-	private String radiant1;
-	private String radiant2;
-	private String radiant3;
-	private String radiant4;
-	private String radiant5;				// indexes are farm priority sensetive eg 1 = safe, 2 = mid...
-	private String dire1;
-	private String dire2;
-	private String dire3;
-	private String dire4;
-	private String dire5;
+	private String rCarry;
+	private String rMid;
+	private String rOfflane;
+	private String rSoft;
+	private String rHard;				// indexes are farm priority sensetive eg 1 = safe, 2 = mid...
+	private String dCarry;
+	private String dMid;
+	private String dOfflane;
+	private String dSoft;
+	private String dHard;
 	
 	public Match() {
 		
 	}
 	
+	public ArrayList<String> getPick() {
+		var pick = new ArrayList<String>();
+		pick.add(rCarry);
+		pick.add(rMid);
+		pick.add(rOfflane);
+		pick.add(rSoft);
+		pick.add(rHard);
+		pick.add(dCarry);
+		pick.add(dMid);
+		pick.add(dOfflane);
+		pick.add(dSoft);
+		pick.add(dHard);
+		return pick;
+	}
+	
+	public void radiantCarry(String hero) {
+		this.rCarry = hero;
+	}
+	public void radiantMid(String hero) {
+		this.rMid = hero;
+	}
+	public void radiantOfflane(String hero) {
+		this.rOfflane = hero;
+	}
+	public void radiantSoft(String hero) {
+		this.rSoft = hero;
+	}
+	public void radiantHard(String hero) {
+		this.rHard = hero;
+	}
+	
+	public void direCarry(String hero) {
+		this.dCarry = hero;
+	}
+	public void direMid(String hero) {
+		this.dMid = hero;
+	}
+	public void direOfflane(String hero) {
+		this.dOfflane = hero;
+	}
+	public void direSoft(String hero) {
+		this.dSoft = hero;
+	}
+	public void direHard(String hero) {
+		this.dHard = hero;
+	}
+
+	public void setID(long id) {
+		this.matchId = id;		
+	}
+
+	public void setDate(String date) {
+		this.date = date;		
+	}
+
 }
--- ProDB/src/main/java/parcing/StratzMatchSniffer.java
+++ ProDB/src/main/java/parcing/StratzMatchSniffer.java
@@ -24,14 +24,22 @@ import data.Match;
 public class StratzMatchSniffer implements MatchSniffer {
 
 	private static final String API_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJodHRwczovL3N0ZWFtY29tbXVuaXR5LmNvbS9vcGVuaWQvaWQvNzY1NjExOTgyODk3OTU3ODgiLCJ1bmlxdWVfbmFtZSI6IkRhcmsgb2YgQ2hhcmdlbmVzcyIsIlN1YmplY3QiOiIwNTZkZDI3Yi03MTAzLTRlMzctYTc0OC1jZmZjMjgwYjViZTgiLCJTdGVhbUlkIjoiMzI5NTMwMDYwIiwibmJmIjoxNjYwMzM3NTI2LCJleHAiOjE2OTE4NzM1MjYsImlhdCI6MTY2MDMzNzUyNiwiaXNzIjoiaHR0cHM6Ly9hcGkuc3RyYXR6LmNvbSJ9.m_s22LBhQgb863YOUDy0zze5k2I0dXgvw9kOGqbQXT0";
+	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0";
 	
 	public Match formById(Long matchId) {
 		
+		String json = fetchJson(matchId);
+		
+		return jsonToMatch(json);
+		
+	}
+	
+	private String fetchJson(long matchId) {
 		RestTemplate restTemplate = new RestTemplate();
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0");
+		headers.add("user-agent", USER_AGENT);
 		headers.add("Authorization", API_TOKEN);
 		
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
@@ -40,40 +48,85 @@ public class StratzMatchSniffer implements MatchSniffer {
 		
 		String json = response.getBody();
 		
+		return json;
+	}
+	
+	private Match jsonToMatch(String json) {
+		
+		Match result = new Match();
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode jNode;
+		
 		try {
+			
 			jNode = mapper.readTree(json);
-			System.out.println(jNode.get("players").get(0).get("heroId"));
-			System.out.println(jNode.get("players").get(1).get("heroId"));
-			System.out.println(jNode.get("players").get(2).get("heroId"));
-			System.out.println(jNode.get("players").get(3).get("heroId"));
-			System.out.println(jNode.get("players").get(4).get("heroId"));
-			System.out.println(jNode.get("players").get(5).get("heroId"));
-			System.out.println(jNode.get("players").get(6).get("heroId"));
-			System.out.println(jNode.get("players").get(7).get("heroId"));
-			System.out.println(jNode.get("players").get(8).get("heroId"));
-			System.out.println(jNode.get("players").get(9).get("heroId"));
+			for(int i = 0; i < 10; i++) {
+				parsePlayer(result, jNode.get("players").get(i));
+			}
+			
+			result.setDate(jNode.get("statsDateTime").asText());
+			result.setID(jNode.get("id").asLong());
+			
 		} catch (JsonProcessingException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
-		
-		
-		try {
-			PrintWriter out = new PrintWriter("output.txt");
-			out.print(response.getBody());
-			out.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		return null;
-		
 	}
 	
+	private void parsePlayer(Match match, JsonNode player) {
+		
+		if(player.get("isRadiant").asBoolean()) {						
+			
+			if(player.get("role").asInt() == 2) {
+				match.radiantHard(player.get("heroId").asText());
+				return;
+			}
+			if(player.get("role").asInt() == 1) {
+				match.radiantSoft(player.get("heroId").asText());
+				return;
+			}
+			if(player.get("lane").asInt() == 1) {						
+				match.radiantCarry(player.get("heroId").asText());
+			} else if(player.get("lane").asInt() == 2) {				
+				match.radiantMid(player.get("heroId").asText());
+			} else {													
+				match.radiantOfflane(player.get("heroId").asText());
+			}
+			
+		} else {														
+			
+			if(player.get("role").asInt() == 2) {
+				match.direHard(player.get("heroId").asText());
+				return;
+			}
+			if(player.get("role").asInt() == 1) {
+				match.direSoft(player.get("heroId").asText());
+				return;
+			}
+			if(player.get("lane").asInt() == 1) {						
+				match.direCarry(player.get("heroId").asText());
+			} else if(player.get("lane").asInt() == 2) {				
+				match.direMid(player.get("heroId").asText());
+			} else {													
+				match.direOfflane(player.get("heroId").asText());
+			}
+			
+		}
+		//*****************************************************************************************
+		// player.role coded as:
+		// 2 = hard
+		// 1 = soft
+		// 0 = core
+		// player.lane coded as:
+		// 1 = bot
+		// 2 = mid
+		// 3 = top
+		// player.side is coded as "isRadiant" boolean
+		// so  radiant carry will be (isRadiant == true, lane = 1, role = 0) etc.
+		//*****************************************************************************************
+	}
+
 	private String urlFromId(long id) {
 		return "https://api.stratz.com/api/v1/match/" + id + "/breakdown";
 	}
